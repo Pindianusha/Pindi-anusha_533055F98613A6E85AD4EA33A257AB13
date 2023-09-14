a0078acaf5702336e92e94a1480ab05136c941cd
Pindianusha <144209266+Pindianusha@users.noreply.github.com>
2023-09-14 19:08:22 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,43 @@
+'''Implement a class called Bank Account that represents a bank account. The class should have private 
+attributes for account number, account holder name, and account balance. Include methods to
+deposit money, withdraw money, and display the account balance. Ensure that the account balance 
+cannot be accessed directly from outside the class. Write a program to create an instance of the 
+Bank Account class and test the deposit and withdrawal functionality.'''
+
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Amount must be greater than zero.")
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        elif amount > self.__account_balance:
+            print("Insufficient funds.")
+        else:
+            print("Invalid withdrawal amount. Amount must be greater than zero.")
+
+    def display_balance(self):
+        print(f"Account balance for {self.__account_holder_name}: ${self.__account_balance}")
+
+
+# Testing the BankAccount class
+if __name__ == "__main__":
+    account = BankAccount("123456", "John Doe", 1000.0)
+
+    account.display_balance()  # Display initial balance
+    account.deposit(500.0)     # Deposit $500
+    account.withdraw(200.0)    # Withdraw $200
+    account.withdraw(1500.0)   # Attempt to withdraw $1500 (insufficient funds)
+    account.deposit(-100.0)    # Attempt to deposit a negative amount
+
+    account.display_balance()  # Display final balance
